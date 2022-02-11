b09a8f21def93874b4f6836f545358fc263a3c31
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-02-11 15:12:19 +1300

    Added welcome function
    Added welcome function and main

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,4 +1,24 @@
 #pizza bot program
+import random
+from random import randint
+
+#list of random names
+names = ["Laurence", "Casey", "Daniel", "Caleb", "Matthew", "James", "Alex", "Zach", "Jayden", "Peter"]
+
+def welcome(): 
+    '''
+    Purpose: to generate a random name from the list and 
+    print out a welcoe message
+    Parameters: none
+    Returns: none
+    '''
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to Dream Pizza ***")
+    print("*** My name is",name, "***")
+    print("*** I will be here to help you order pizza's of your chioce ***")
+
+
 
 #Welcome message with random name
 
@@ -67,3 +87,12 @@
 
 
 #main function
+def main():
+    '''
+    Purpose: To run all runctions.      
+    Parameters: none
+    Returns: none
+    '''
+    welcome()
+
+main()
