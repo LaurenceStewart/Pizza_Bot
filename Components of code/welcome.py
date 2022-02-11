b565eb2287b6c9b1955d76fe2d2674537041a0f2
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-02-11 14:33:31 +1300

    Component file and welcome
    Welcome component created v1

---
+++ Components of code/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#list of random names
+names = ["Laurence", "Casey", "Daniel", "Caleb", "Matthew", "James", "Alex", "Zach", "Jayden", "Peter"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to Dream Pizza ***")
+print("*** My name is",name, "***")
+print("*** I will be here to help you order pizza's of your chioce ***")
