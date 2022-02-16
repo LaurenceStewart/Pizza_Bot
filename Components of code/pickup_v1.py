1ed3d2c1a76eb595059dd2684a1bec5d6f9cd23c
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-02-17 12:54:41 +1300

    Created v1 of pickup option
    Instructions so customer can enter name and phone number

---
+++ Components of code/pickup_v1.py
@@ -0,0 +1,13 @@
+
+
+print("Please enter the pickup information")
+
+#customer name
+name = input("Please enter you name")
+
+
+#customer phone number
+phone = input("Please enter you phone number")
+
+print (name)
+print (phone)
