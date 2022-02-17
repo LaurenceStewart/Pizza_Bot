60334dcc53d33796df262692a1665d132d6c7252
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-02-17 13:13:49 +1300

    Code does not accept blank input
    Code not accept blank input for phone number or address information

---
+++ Components of code/pickup_v2.py
@@ -0,0 +1,25 @@
+#Bug accepts blank input
+
+print("Please enter the pickup information")
+
+#customer name
+valid = False
+while not valid:
+    name = input("Please enter you name")
+    if name != "":
+        print (name)
+        break
+    else:
+        print("Sorry this can not be blank")
+
+
+
+#customer phone number
+valid = False
+while not valid:
+    phone = input("Please enter you phone number")
+    if phone != "":
+        print (phone)
+        break
+    else:
+        print("Sorry this can not be blank")
