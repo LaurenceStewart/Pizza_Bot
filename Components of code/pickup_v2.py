6f6efe61826ad2c04d4aa257e91cc340f6a57f44
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-02-21 13:02:07 +1300

    Added customer details dictionary
    Added a dictionary to store customer details.
    Name, and phone

--- Components of code/pickup_v2.py
+++ Components of code/pickup_v2.py
@@ -5,21 +5,21 @@ print("Please enter the pickup information")
 #customer name
 valid = False
 while not valid:
-    name = input("Please enter you name")
+    name = input("Please enter you name ")
     if name != "":
         print (name)
         break
     else:
-        print("Sorry this can not be blank")
+        print("Sorry this cannot be blank")
 
 
 
 #customer phone number
 valid = False
 while not valid:
-    phone = input("Please enter you phone number")
+    phone = input("Please enter you phone number ")
     if phone != "":
         print (phone)
         break
     else:
-        print("Sorry this can not be blank")
+        print("Sorry this cannot be blank")
