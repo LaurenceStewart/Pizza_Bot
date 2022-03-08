20f1c2f5163a4efdb9a175a9860909fdae2baf76
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-03-08 18:54:54 +1300

    Added validation to number of pizzas
    Added validation to number of pizzas, only accept number between 1 and 5 and only accept integer

--- Components of code/pizza_order_v2.py
+++ Components of code/pizza_order_v2.py
@@ -22,7 +22,17 @@ menu()
 #ask total number of pizzas for order
 num_pizzas = 0
 
-num_pizzas = int(input("How many pizzas do you want to order? "))
+
+while True:
+    try:
+        num_pizzas = int(input("How many pizzas do you want to order? "))
+        if num_pizzas >= 1 and num_pizzas <=5:
+            break
+        else:
+            print ("Your order must be between 1 and 5 pizzas")
+    except ValueError:
+        print("That is not a valid number")
+        print("Please enter a number between 1 and 5")      
 
 print (num_pizzas)
 
@@ -31,12 +41,13 @@ print ("Please choose you pizza by entering the number from the menu")
 for item in range(num_pizzas):
     while num_pizzas > 0:
         pizza_ordered = int(input())
+        pizza_ordered = pizza_ordered -1
         order_list.append(pizza_names[pizza_ordered])
         order_cost.append(pizza_prices[pizza_ordered])
+        print("{} ${:.2f}"  .format(pizza_names[pizza_ordered],pizza_prices[pizza_ordered]))
         num_pizzas = num_pizzas-1
 
-print(order_list)
-print(order_cost)
+
 
 
 #Count down until all pizzas are ordered
