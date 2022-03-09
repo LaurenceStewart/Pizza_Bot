218aa2ed8ff86b6db10a279b7e7deee594d0ddb5
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-03-10 11:14:07 +1300

    Customer details to print
    Added to code to print customer details

---
+++ Components of code/order_print_v2.py
@@ -0,0 +1,18 @@
+order_list = ['Margherita','Hawaiian','Vegan','BBQ Chicken Deluxe']
+#list to store ordered prices
+order_cost = [8.50, 8.50, 8.50, 13.50]
+
+cust_details = {'name':'Laurence', 'phone':'0219843298', 'house':'23', 'street':'Evelyn', 'suburb':'Howick'}
+
+
+
+
+#print("\n",cust_details['name'], "\n",cust_details['phone'], "\n",cust_details['house'], "\n",cust_details['street'], "\n",cust_details['suburb'])
+
+print("\n Customer name: {} Custoner Phone:\n{} Customer House Number:\n{} Customer Street Name:\n{} Customer Suburb:\n{}".format( cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb']))
+
+
+count = 0
+for item in order_list:
+    print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+    count = count+1
